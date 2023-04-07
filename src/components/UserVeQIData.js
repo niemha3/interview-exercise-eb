c7533ee42a946611bb629562e158a24bdc389069
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-04-07 14:44:26 +0300

    Add component to show users veqi data

---
+++ src/components/UserVeQIData.js
@@ -0,0 +1,24 @@
+//TODO: Add veQI votes to table as props
+
+const UserVeQIData = ({ veQiBalance, veQiVotes}) => {
+    return(
+        <div>
+            <table className="table-auto w-full mt-8">
+      <thead>
+        <tr>
+          <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">veQI Balance</th>
+          <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">veQI Votes</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="border px-4 py-2 text-sm font-medium">{veQiBalance}</td>
+          <td className="border px-4 py-2 text-sm font-medium">{veQiVotes}</td>
+        </tr>
+      </tbody>
+    </table>
+        </div>
+    )
+}
+
+export default UserVeQIData
