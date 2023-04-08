4289ee1d48e7c55683825ac34e3386488c2f3229
Harri Nieminen <harrinieminenkoodaargmail.com>
2023-04-08 10:48:15 +0300

    Add function to fetch nodes and to get nodes votes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import veQIErc20ABI from './veQIErc20ABI.json'
 import gaugeControllerErc20ABI from './gaugeControllerErc20ABI.json'
 import CryptoCard from './components/CryptoCard'
 import UserVeQIData from './components/UserVeQIData'
-import BigNumber from 'bignumber.js'
+
 
  const App = () => {
 
   const veQiProxyContract = "0x7Ee65Fdc1C534A6b4f9ea2Cc3ca9aC8d6c602aBd"
-  const addressOfMainContract = "0xeb4eE988D975A91f02884076717D5EeACb41976f"
+  // const addressOfMainContract = "0xeb4eE988D975A91f02884076717D5EeACb41976f"
 
   const gaugeControllerProxyContract ="0x14593cb3Ffe270a72862Eb08CeB57Bc3D4DdC16C"
 
@@ -20,21 +20,69 @@ import BigNumber from 'bignumber.js'
   const [wallet, setWallet] = useState('')
   const [veQiBalance, setVeQiBalance] = useState(null)
   const [veQiVotes, setVeQiVotes] = useState(null)
+  const [nodesList, setNodesList] = useState([])
+  const [nodes, setNodes] = useState([])
+
+
   
 
- 
 
   useEffect(() => {
+
+    const provider = new ethers.providers.JsonRpcProvider("https://api.avax.network/ext/bc/C/rpc")
+    const veQicontract = new ethers.Contract(veQiProxyContract, veQIErc20ABI, provider) 
+    const gaugeControllerContract = new ethers.Contract(gaugeControllerProxyContract, gaugeControllerErc20ABI, provider)
+
+    /**
+     * Get list of all nodes and set it to nodes state
+     */
+    const fetchListOfNodes = async () => {    
+      const nodeCount = await gaugeControllerContract.getNodesLength()
+      console.log(nodeCount.toString())
+      const nodeIds = await gaugeControllerContract.getNodesRange(0, (nodeCount - 1).toString())
+
+      setNodes(nodeIds)
+      console.log("Nodes list: ", nodes)
+  
+
+     }
+
+
+     /**
+      * Get votes for one node
+      */
+     const fetchVotesForNode = async () => {
+
+      const votesForNode = await gaugeControllerContract.getVotesForNode(nodes[0])
     
-    const fetchData = async () => {
+      // const nod = nodes.map(async (nodeId) => {
+      //   const votesForNode = await gaugeControllerContract.getVotesForNode(nodeId)
+      //   nodeObjects.push({
+      //     nodeName: nodeId,
+      //     votes: votesForNode.toString()
+
+      //   })
+      // })
+
+      
+      console.log("Votes for node: ", votesForNode.toString())
+     }
+
+     const getRelativeWeightOfNode = async () => {
     
-      const provider = new ethers.providers.JsonRpcProvider("https://api.avax.network/ext/bc/C/rpc")
-      const veQicontract = new ethers.Contract(veQiProxyContract, veQIErc20ABI, provider) 
-      const gaugeControllerContract = new ethers.Contract(gaugeControllerProxyContract, gaugeControllerErc20ABI, provider)
+      // const relativeWeight = await gaugeControllerContract.getRelativeWeightOfNode(nodes[0])
+      // const voteCounts = nodesList.map(async (node)  => {
+      //    await gaugeControllerContract.getVotesForNode(node)
+      // })
+  
+      // console.log("Relative weight of node:", relativeWeight )
+    }
     
-    
-      // Name of the token
-      const tokenName = await veQicontract.name()
+    fetchListOfNodes()
+    fetchVotesForNode()
+    // getRelativeWeightOfNode()
+
+    const fetchUsersData = async () => {
 
       // Users veQI balance
       const userVeQiBalance = await veQicontract.balanceOf(wallet)
@@ -46,22 +94,21 @@ import BigNumber from 'bignumber.js'
       const userVeQiVotes = await gaugeControllerContract.getUserVotesLength()
 
       setVeQiVotes(userVeQiVotes.toString())
-
-      //TODO: Total vote count for each validator
-
-      //TODO: relative weight for each validaer
-
-    console.log("veQIContract ", veQicontract)
-    console.log("Your veQI balance ", userVeQiBalance)
-    
-    console.log("Token name", tokenName)
     
   }
   
   if(wallet) {
-    fetchData()
+    fetchUsersData()
   }
-},[wallet])
+},[wallet, nodesList])
+
+
+
+
+
+       //TODO: relative weight for each validaer
+
+
 
 
   
@@ -91,14 +138,14 @@ import BigNumber from 'bignumber.js'
 
   return (
     <div className="App bg-gradient-to-b from-gray-900 to-blue-900
-    h-screen text-gray-100 font-serif w-auto h-screen ">
+    h-screen text-gray-100 font-serif w-auto h-auto">
 
       <div className="flex flex-col justify-center items-center py-3 px-8 w-30 h-10 mb-20 ">
         <img className="object-cover h-48 w-96 mt-20" alt="logo" src="./BenqiWordmarkWhite.png" />
         </div>
 
   
-      <div className="flex flex-col justify-center items-center py-20"> 
+      <div className="flex flex-col justify-center items-center py-20 bg-gradient-to-b from-gray-900 to-blue-900"> 
 
         {!wallet && <button className=" text-gray-100 bg-gradient-to-r from-cyan-500 to-blue-500 border-transparent rounded  shadow-md border-2 border-gray-100 py-2 px-2 my-2" onClick={requestAccount}>Connect to wallet</button> }
 
@@ -107,6 +154,12 @@ import BigNumber from 'bignumber.js'
             <CryptoCard walletAddress={wallet} />
             <UserVeQIData veQiBalance={veQiBalance} veQiVotes={veQiVotes} />
           </> }
+        </div>
+
+        <div className="nodes bg-gradient-to-b from-gray-900 to-blue-900">
+          {nodes.map(node => (
+            <p key={node}>{node}</p>
+          ))}
         </div>
     </div>
   );
